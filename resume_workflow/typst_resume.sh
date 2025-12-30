a8ce7d2fcba02822f62aa3490438d64c1f529285
Francois-Coleongco <christien.coleongco@gmail.com>
2025-12-29 22:14:14 -0700

    new file structure cuz of encryption

--- resume_workflow/typst_resume.sh
+++ resume_workflow/typst_resume.sh
@@ -4,7 +4,7 @@ your_home="/home/soy"
 
 
 ${your_home}/.local/kitty.app/bin/kitty --detach sh -c "
-tmux new-session -d -s resume -c ${your_home}/notes/resume/pipeline/basic-resume/ '${your_home}/.cargo/bin/typst watch ${your_home}/notes/resume/pipeline/basic-resume/main.typ'; \
-tmux new-window '/usr/bin/zathura ${your_home}/notes/resume/pipeline/basic-resume/main.pdf'; \
-tmux new-window 'cd ${your_home}/notes/resume/pipeline/basic-resume/; /opt/nvim-linux-x86_64/bin/nvim'; \
+tmux new-session -d -s resume -c ${your_home}/notes/data/resume/pipeline/basic-resume/ '${your_home}/.cargo/bin/typst watch ${your_home}/notes/data/resume/pipeline/basic-resume/main.typ'; \
+tmux new-window '/usr/bin/zathura ${your_home}/notes/data/resume/pipeline/basic-resume/main.pdf'; \
+tmux new-window 'cd ${your_home}/notes/data/resume/pipeline/basic-resume/; /opt/nvim-linux-x86_64/bin/nvim'; \
 tmux attach -t resume"
