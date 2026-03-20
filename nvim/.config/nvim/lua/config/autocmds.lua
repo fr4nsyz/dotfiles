3f5a4b73ee434ddc60a74045f6e4a7c518b0fd2c
fr4nsyz <christien.coleongco@gmail.com>
2026-03-20 17:02:05 -0400

    latex autocmds

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -7,4 +7,19 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.opt.wrap = true
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "VimtexEventInitPost",
+  callback = function()
+    vim.cmd("VimtexCompile")
+  end,
+  once = true,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "tex",
+  callback = function()
+    vim.keymap.set("n", "<leader>r", function()
+      vim.cmd("!pdflatex %")
+    end, { buffer = true, silent = true })
+  end,
+})
